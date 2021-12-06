d5ad7306c6467c1e99ceb82f2c0da1b4c419d546
Alejandro Enríquez <alexalejandroem@gmail.com>
2021-12-06 15:13:12 +0100

    Added initial post status Added error post status

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
+import 'package:pos/bloc/products/product_event.dart';
+import '../bloc/products/product_bloc.dart';
+import '../bloc/products/product_state.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    context.read<ProductBloc>().add(ProductsFetched());
     DateTime now = DateTime.now();
     String hour = DateFormat('kk:mm').format(now);
     String day = DateFormat('EEEE, dd MMM').format(now);
@@ -27,8 +32,27 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
-      body: const Center(
-        child: Text('Home'),
+      body: Center(
+        child: BlocBuilder<ProductBloc, ProductState>(
+          builder: (BuildContext context, ProductState state) {
+            switch (state.status) {
+              case ProductStatus.failure:
+                return const Center(child: Text('failed to fetch posts'));
+              case ProductStatus.success:
+                if (state.products.isEmpty) {
+                  return const Center(child: Text('no posts'));
+                }
+                return ListView.builder(
+                  itemBuilder: (BuildContext context, int index) {
+                    return Text(state.products[index].name);
+                  },
+                  itemCount: state.products.length,
+                );
+              default:
+                return const Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
       ),
     );
   }
